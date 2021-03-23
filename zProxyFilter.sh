9c3c9778db1b9d377279bee95dd066d75acc97c6
zGumeloBr <66852780+zGumeloBr@users.noreply.github.com>
2021-03-23 11:24:47 -0300

    Update zProxyFilter.sh

--- zProxyFilter.sh
+++ zProxyFilter.sh
@@ -6,51 +6,84 @@
 #
 #
 
-#Verificar se o script vai ser executado em root ou não.
+#Verificações importantes
 
 if [[ $EUID -ne 0 ]]; then
-  echo -e "\033[1;32m * Esse script precisa ser executado em super usuário (sudo). \033[0m" 1>&2
+  echo -e "\033[1;31m * Esse script precisa ser executado em super usuário (sudo). \033[0m" 1>&2
   exit 1
 
 fi
 
-# Dependências para funcionamento do script.
+if ! [ -x "$(command -v wget)" ]; then
+  echo "\033[1;31m * O comando (wget) não foi encontrado, por favor instale-o\033[0m"
+  exit 1
+fi
 
-sudo apt -y update
+if ! [ -x "$(command -v iptables)" ]; then
+  echo "\033[1;31m * O comando (iptables) não foi encontrado, por favor instale-o\033[0m"
+  exit 1
+fi
 
-sudo apt -y upgrade
+if ! [ -x "$(command -v ipset)" ]; then
+  echo "\033[1;31m * O comando (ipset) não foi encontrado, por favor instale-o\033[0m"
+  exit 1
+fi
 
-sudo apt install -y wget
+# Criando tabela para bloquear as proxies.
 
-sudo apt install -y iptables
+ipset create zProxyFilter -exist hash:ip hashsize 9999999 maxelem 9999999 timeout 0
 
-sudo apt install -y ipset
+#
+#
+# Metódo (MPB)
+#
+#
 
-ipset create zShield -exist hash:ip hashsize 9999999 maxelem 9999999 timeout 0
+wget -O mpb.toml https://raw.githubusercontent.com/zGumeloBr/zMPB-Module/main/proxies.txt
 
-# Plataformas de obtenção das proxies.
+#
+#
+# Metódo (APB)
+#
+#
 
-wget -O proxyS4.txt https://www.proxy-list.download/api/v1/get?type=socks4
+wget -O pl1.toml https://www.proxy-list.download/api/v1/get?type=socks4
 
-wget -O PSproxies.txt https://api.proxyscrape.com/?request=displayproxies&proxytype=all&timeout=all&country=all
+wget -O pl2.toml https://api.proxyscrape.com/?request=displayproxies&proxytype=all&timeout=all&country=all
 
-wget -O githubList.txt https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/nixspam.ipset
+wget -O pl3.toml https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/nixspam.ipset
 
-wget -O tor.txt https://check.torproject.org/torbulkexitlist
+wget -O pl4.toml https://check.torproject.org/torbulkexitlist
 
-wget -O proxyS5.txt https://www.proxy-list.download/api/v1/get?type=socks5
+wget -O pl5.toml https://www.proxy-list.download/api/v1/get?type=socks5
 
-wget -O psS4.txt https://www.proxyscan.io/download?type=socks4
+wget -O pl6.toml https://www.proxyscan.io/download?type=socks4
 
-wget -O psS5.txt https://www.proxyscan.io/download?type=socks5
+wget -O pl7.toml https://www.proxyscan.io/download?type=socks5
 
-wget -O proxyscrap.txt https://api.proxyscrape.com/v2/?request=getproxies
+wget -O pl8.toml https://api.proxyscrape.com/v2/?request=getproxies
 
-# Compactando todos os arquivos em 1 so.
+wget -O pl9.toml https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt
 
-cat proxyS4.txt > proxies.txt | cat proxyS5.txt > proxies.txt | cat psS4.txt > proxies.txt | cat psS5.txt > proxies.txt | cat proxyscrap.txt > proxies.txt | cat tor.txt > proxies.txt | cat githubList.txt > proxies.txt
+wget -O pl10.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt
 
-# Replace para retirar as portas
+wget -O pl11.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt
+
+wget -O pl12.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/https.txt
+
+wget -O pl13.toml https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt
+
+wget -O pl14.toml https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt
+
+wget -O pl15.toml https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt
+
+wget -O pl16.toml https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt
+
+# Compactando todas as proxies em um arquivo.
+
+cat mpb.toml pl1.toml pl2.toml pl3.toml pl4.toml pl5.toml pl6.toml pl7.toml pl8.toml pl9.toml pl10.toml pl11.toml pl12.toml pl13.toml pl14.toml pl15.toml pl16.toml >> proxies.txt
+
+# Replace para retirar as portas e caracteres inuteis.
 
 sed -i 's/$/:/' proxies.txt
 
@@ -60,18 +93,24 @@ sed -i 's/[:]\+//g' proxies.txt
 
 sed -i '/^#/d' proxies.txt
 
-#Apagando os arquivos inuteis.
+sed -i '/^$/d' proxies.txt
 
-rm -rf proxyS4.txt | rm -rf tor.txt | rm -rf PSproxies.txt | rm -rf proxyS5.txt | rm -rf psS4.txt | rm -rf psS5.txt | rm -rf proxyscrap.txt | rm -rf githubList.txt
+sed -n '/^This/d' proxies.txt
 
 # Bloqueando todos as proxys coletadas.
 
 for x in $(cat proxies.txt)
 do
-        ipset -A zShield $x
+        ipset -A zProxyFilter $x
 done
 
-iptables -I INPUT -m set --match-set zShield src -j DROP
+iptables -I INPUT -m set --match-set zProxyFilter src -j DROP
+
+#Limpando arquivos de proxies antigos
+
+rm *.toml
+
+rm proxies.txt
 
 #Limpando terminal
 
@@ -98,7 +137,3 @@ for x in $(wc -l proxies.txt)
 do
         echo "⋄ | $x"
 done
-
-#Limpando lista de proxies antiga
-
-rm -rf proxies.txt
